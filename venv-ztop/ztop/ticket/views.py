1334da2826768de2d3bb8551fcf433b1daf03299
CDMelendez <cdmelendez_@hotmail.com>
2022-11-10 19:18:08 -0800

    Ticket Generation
    - Added ticket generation page

---
+++ venv-ztop/ztop/ticket/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    #return HttpResponse("Ticket Generation Form Goes Here:")
+    if(request.method=='POST'):
+        ID_req = request.POST.get('name')
+        ID_tech = request.POST.get('name')
+        assigned = models.BooleanField()
+        dateCreation = models.DateTimeField('Date created')
+        dateAssign = models.DateTimeField('Date assigned', null=True, blank=True)
+        dateFinish = models.DateTimeField('Date finished', null=True, blank=True)
+
+    return render(request, 'index.html')
