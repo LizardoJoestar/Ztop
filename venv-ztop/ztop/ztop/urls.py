7f2917ee29f62200b1db2f7bc08b663fead41f8d
CDMelendez <cdmelendez_@hotmail.com>
2022-12-01 16:33:58 -0800

    Merge with dev2
    - Merged the changes made in develop with dev2

--- venv-ztop/ztop/ztop/urls.py
+++ venv-ztop/ztop/ztop/urls.py
@@ -18,7 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls'))
+    path('polls/', include('polls.urls')),
     path('ticket/', include('ticket.urls'))
 ]
 
